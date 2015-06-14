b5c86b5e83695eb88ce4ecb2ee749f9b6fb594d6
Jeff Richardson <jeffreywebdevelopment@gmail.com>
2015-06-14 12:32:32 -0500

    init commit

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+// Gruntfile.js
+module.exports = function(grunt){
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    watch: {
+      scripts: {
+        files: '**/*.js',
+        tasks: ['uglify'],
+        options: {
+          interrupt: true,
+        },
+      },
+    },
+
+    jshint: {
+      basic: ['src/main.js']
+    },
+
+    mocha: {
+      all: {
+        src: ['tests/testrunner.html'],
+      },
+      options: {
+        run: true
+      }
+    },
+    uglify: {
+      basic: {
+        files: {
+          'dist/marketingform-min.js': ['src/main.js']
+        }
+      }
+    }
+  });
+
+  // Load grunt mocha task
+  grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['jshint', 'mocha', 'uglify']);
+};
